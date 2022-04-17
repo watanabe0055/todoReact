49e02032d68d432211457bf6a494cf55768094c2
watanabe0055 <103058543+watanabe0055@users.noreply.github.com>
2022-04-18 00:00:41 +0900

    markupHTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import React from "react";
-import "./style.css";
+import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを追加" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <div>
+          <ul>
+            <li>aaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <div>
+          <ul>
+            <li>oooo</li>
+            <button>戻す</button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 
 import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
