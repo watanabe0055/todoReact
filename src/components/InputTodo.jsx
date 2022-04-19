92e005aa7040986ae0e08b7ff4eba3e26ecfaab3
watanabe0055 <103058543+watanabe0055@users.noreply.github.com>
2022-04-20 02:49:24 +0900

    CSS IN JSの学習

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "50px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+  lineHeight: "30px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input value={todoText} onChange={onChange} placeholder="TODOを追加" />
       <button onClick={onClick}>追加</button>
     </div>
